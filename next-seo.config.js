afa22b3ccf7f99a5831fd115fadf9b12c941394b
Cyril <cyrildeveloppeur@gmail.com>
2021-02-22 16:04:02 +0100

    next-seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,14 +4,20 @@ export default {
   openGraph: {
     type: "website",
     url: "https://www.cyrilo.dev/",
-    title: "Open Graph Title",
+    title: "Cyrilo Dev",
     description: "Cyrilo Dev | Fullstack JavaScript Developper | France",
     images: [
       {
-        url: "https://cyrilo.dev/images/CyriLogo-OG.svg",
+        url: "https://cyrilo.dev/images/CyriLogo-OG.png",
         width: 1200,
         height: 630,
         alt: "Og Image Alt",
+      },
+      {
+        url: "https://www.example.ie/og-image-2.jpg",
+        width: 800,
+        height: 600,
+        alt: "Og Image Alt 2",
       },
     ],
   },
